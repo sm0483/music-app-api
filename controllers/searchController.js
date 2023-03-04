acd97797269da24a0b1c7367b68e7746b77983bc
sreerag <msratofficial@gmail.com>
2023-03-04 21:24:58 +0530

    refactor:refactor search route

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,70 +1,13 @@
 // search bar request from client side
 const asyncWrapper = require('../error/asyncWrapper');
 const { StatusCodes } = require("http-status-codes");
-const Admin = require("../models/admin");
-const Genre = require("../models/genre");
-const Song=require("../models/music");
-const Language = require("../models/language");
-const Album=require("../models/album");
+const {
+    searchMusicByGenre,
+    searchMusicByLanguage,
+    searchMusicByName,
+    searchAlbum
+}=require("../utils/searchMusic");
 
-// search genre
-const searchMusicByGenre=async(genreName)=>{
-  try {
-    const genre = await Genre.findOne({ genreName:new RegExp('^'+genreName,'i') });
-    if (!genre) return null;
-    const song = await Song.find({ genres: { $in: [genre._id] } }).populate({
-        path:'artistName',
-        select:"name -_id"
-    }).populate({
-        path:'albumName',
-        select:"albumName -_id"
-    }).exec();
-    return song;
-  } catch (err) {
-    return null
-  }
-}
-
-// search by language
-const searchMusicByLanguage=async(languageName)=>{
-    try {
-        const language=await Language.findOne({languageName:new RegExp('^'+languageName,'i')});
-        if(!language) return null;
-        const song=await Song.find({language:language._id}).populate({
-            path:'artistName',
-            select:"name -_id"
-        }).populate({
-            path:'albumName',
-            select:"albumName -_id"
-        }).exec();
-        return song;
-    } catch (error) {
-        return null;
-        
-    }
-}
-
-// search play list name
-
-const searchAlbum=async(albumName)=>{
-    try {
-        const album=await Album.find({albumName: new RegExp('^'+albumName,'i')});
-        if(!album) return null;
-        return album;
-    } catch (error) {
-        return null;
-    }
-}
-
-const searchMusicByName=async(songName)=>{
-    try{
-        const song = await Song.find({ songName: new RegExp('^'+songName, 'i') });
-        return song;
-    }catch(err){
-        console.log(err);
-        return null;
-    }
-}
 
 const getResult=asyncWrapper(async(req,res)=>{
     const query=req.params.query;
@@ -76,7 +19,7 @@ const getResult=asyncWrapper(async(req,res)=>{
     ])
 
     const searchResult=searchResponse.filter((result)=>{
-        if((result!==null || result!==undefined) && (result && result.length!==0)) return result;
+        if((result!==null || result!==undefined) && (result && result.length!==0))return result;
     })
 
     res.status(StatusCodes.OK).json(searchResult)
@@ -84,10 +27,6 @@ const getResult=asyncWrapper(async(req,res)=>{
 })
 
 
-//search song by album id
-//search song by playlist id
-//search song by artist id
-// get 25 song links in an array
 
 module.exports={
     getResult
---
+++ utils/searchMusic.js
@@ -0,0 +1,81 @@
+const Genre = require("../models/genre");
+const Song = require("../models/music");
+const Language = require("../models/language");
+const Album = require("../models/album");
+
+// search genre
+const searchMusicByGenre = async (genreName) => {
+  try {
+    const genre = await Genre.findOne({
+      genreName: new RegExp("^" + genreName, "i"),
+    });
+    if (!genre) return null;
+    const song = await Song.find({ genres: { $in: [genre._id] } })
+      .populate({
+        path: "artistName",
+        select: "name -_id",
+      })
+      .populate({
+        path: "albumName",
+        select: "albumName -_id",
+      })
+      .exec();
+    return song;
+  } catch (err) {
+    return null;
+  }
+};
+
+// search by language
+const searchMusicByLanguage = async (languageName) => {
+  try {
+    const language = await Language.findOne({
+      languageName: new RegExp("^" + languageName, "i"),
+    });
+    if (!language) return null;
+    const song = await Song.find({ language: language._id })
+      .populate({
+        path: "artistName",
+        select: "name -_id",
+      })
+      .populate({
+        path: "albumName",
+        select: "albumName -_id",
+      })
+      .exec();
+    return song;
+  } catch (error) {
+    return null;
+  }
+};
+
+// search play list name
+
+const searchAlbum = async (albumName) => {
+  try {
+    const album = await Album.find({
+      albumName: new RegExp("^" + albumName, "i"),
+    });
+    if (!album) return null;
+    return album;
+  } catch (error) {
+    return null;
+  }
+};
+
+const searchMusicByName = async (songName) => {
+  try {
+    const song = await Song.find({ songName: new RegExp("^" + songName, "i") });
+    return song;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+module.exports = {
+  searchMusicByGenre,
+  searchMusicByLanguage,
+  searchMusicByName,
+  searchAlbum,
+};
